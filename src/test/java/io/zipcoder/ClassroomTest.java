c733aaac218b8e6f7d83992432b0377d889f2d55
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-09 21:47:43 -0500

    test structure draft done

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,47 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class ClassroomTest {
+    @Test
+    public void getStudentsTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void addStudentTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void getAverageExamScoreTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void removeStudentTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void getStudentsByScoreTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void getGradebookTest(){
+        //given
+        //when
+        //then
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,55 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class StudentTest {
 
+    @Test
+    public void setFirstNameTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void setLastNameTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void addExamScoreTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void setExamScoreTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void getNumbersOfExamsTakenTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void getAverageExamScoreTest(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void toStringTest(){
+        //given
+        //when
+        //then
+    }
 }
